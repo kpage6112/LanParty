1f455bbf15f88fd5af7966d4860537317f6d430b
KRISTEN McIntosh <KPAGE6112@my.sctd.edu>
2018-01-17 21:26:08 -0500

    Add project files.

---
+++ LanParty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LanParty.Startup))]
+namespace LanParty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
